86c972ebd0e89e5031ee784535e952fe1afade66
FacelessAchilas <47067200+FacelessAchilas@users.noreply.github.com>
2019-01-27 14:55:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -109,19 +109,7 @@ bot.on('message', function(message){
      message.channel.send(emote) 
     }
     
-        if(message.content == "ten!Random:")
-        {
-            
-            
-    var str = message.content;
-    var value = str.match(/#random=(\d+)/i)[1];
-
-       message.channel.send(value)     
-            
-            
-            
-        }
-    
+       
 });
 
 bot.on('ready', function(){
